bd9b6cd2f03c76bd9474365a2b6896df63f363ec
Mansi <mansisharma209aps@gmail.com>
2023-10-11 01:34:23 +0530

    Footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Footer from "../shared/Footer.svelte";
+  import Navbar from "../shared/Navbar.svelte";
+</script>
+
+<!-- <Footer /> -->
+<!-- Fetch this common component in the webpages -->
