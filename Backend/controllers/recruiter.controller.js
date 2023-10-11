fb3345cef9eb82e406665a4e6494d27d40a901e1
Vighnesh Manjrekar <vighneshmanjrekar4193@gmail.com>
2023-10-12 00:52:50 +0530

    Update Recuiter Schema

--- Backend/controllers/recruiter.controller.js
+++ Backend/controllers/recruiter.controller.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 const asyncHandler = require("../middleWare/async");
 const prisma = require("../utils/prisma");
 const ErrorResponse = require("../utils/ErrorResponse");
@@ -9,18 +9,18 @@ const sendToken = (user, statusCode, res) => {
     expiresIn: process.env.JWT_EXPIRE,
   });
 
-  //   const secure = process.env.NODE_ENV == "production" ? true : false;
-  //   const options = {
-  //     expires: new Date(
-  //       Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-  //     ),
-  //     httpOnly: true,
-  //     secure,
-  //   };
+  const secure = process.env.NODE_ENV == "production" ? true : false;
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    secure,
+  };
 
   res
     .status(statusCode)
-    // .cookie("token", token, options)
+    .cookie("token", token, options)
     .json({ success: true, token });
 };
 
@@ -28,21 +28,54 @@ const sendToken = (user, statusCode, res) => {
 // @route   POST api/v1/recruiter/auth/register
 // @access  Public
 exports.register = asyncHandler(async (req, res, next) => {
-  const { username, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
+  const { first_name, last_name, Email_verified, Password } = req.body;
+  const requiredFields = [
+    "first_name",
+    "last_name",
+    "Email_verified",
+    "Password",
+  ];
+  const emptyFields = requiredFields.filter((field) => !req.body[field]);
+  if (emptyFields.length > 0) {
+    return next(new ErrorResponse(400, `Missing ${emptyFields.join(", ")}`));
+  }
+
+  const hashedPassword = await bcrypt.hash(Password, 10);
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const passwordRegex =
+    /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
+
+  if (!emailRegex.test(Email_verified)) {
+    return next(
+      new ErrorResponse(400, `${Email_verified} is an invalid email`)
+    );
+  }
+
+  if (!passwordRegex.test(Password)) {
+    return next(
+      new ErrorResponse(
+        400,
+        `Password must be at least 8 characters long and contain an uppercase letter, a number, and a special character`
+      )
+    );
+  }
 
   const user = await prisma.recruiter.findUnique({
-    where:{
-        username
-    }
-  })
+    where: {
+      Email_verified,
+    },
+  });
 
+  if (user) {
+    return next(new ErrorResponse(400, `Email already exists`));
+  }
 
   await prisma.recruiter.create({
-    data: { username, password: hashedPassword },
+    data: { first_name, last_name, Email_verified, Password: hashedPassword },
   });
   return res
-    .status(200)
+    .status(201)
     .json({ success: true, message: "User created successfully" });
 });
 
@@ -50,26 +83,19 @@ exports.register = asyncHandler(async (req, res, next) => {
 // @route   POST api/v1/auth/login
 // @access  Public
 exports.login = asyncHandler(async (req, res, next) => {
-  const { username, password } = req.body;
+  const { Email_verified, Password } = req.body;
 
-  if (!username || !password) {
+  if (!Email_verified || !Password) {
     return next(new ErrorResponse(400, "Please enter email and password"));
   }
 
   const user = await prisma.recruiter.findUnique({
     where: {
-      username,
+      Email_verified,
     },
   });
 
-  
-  if (!user) {
-    return next(new ErrorResponse(401, "Invalid Username"));
-  }
-
-  const isValid = await bcrypt.compare(password, user.password);
-
-  if (!isValid) {
+  if (!user || !(await bcrypt.compare(Password, user.Password))) {
     return next(new ErrorResponse(401, "Invalid Credentials"));
   }
 
--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,8 @@
-require("dotenv").config();;
+require("dotenv").config();
 const express = require("express");
 // const bodyParser = require("body-parser");
 
-const recruiterRouter = require("./routes/recruiter.router"); 
+const recruiterRouter = require("./routes/recruiter.router");
 const errorHandler = require("./middleWare/error");
 
 const app = express();
@@ -10,20 +10,20 @@ const app = express();
 // app.use(bodyParser.json()); // no need to use this
 app.use(express.json());
 
-app.use("/api/v1/recruiter/auth", recruiterRouter)
-app.use(errorHandler)
+app.use("/api/v1/recruiter/auth", recruiterRouter);
+app.use(errorHandler);
 
 // APIs endpoint
 
 // Route not found
 app.use((req, res, next) => {
-    console.log("ATS -> URL not Found || Requested URL -  " + req.url);
-    return res.status(404).send("404 not found");
+  console.log("ATS -> URL not Found || Requested URL -  " + req.url);
+  return res.status(404).send("404 not found");
 });
 
 const port = process.env.PORT || 5000;
 
 // listen
 app.listen(port, () => {
-    console.log("ATS -> Server is listining on port " + port);
+  console.log("ATS -> Server is listining on port " + port);
 });
