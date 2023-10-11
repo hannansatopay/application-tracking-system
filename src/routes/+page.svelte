a4eb9a208d31c5538d14c3c6c8caff41a3a67e04
Mansi <mansisharma209aps@gmail.com>
2023-10-11 23:20:33 +0530

    Sign up page and Job Post Page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,14 @@
 <script>
   import Home from "../pages/Home.svelte";
+  import JobPost from "../pages/JobPost.svelte";
+  import SignUp from "../pages/SignUp.svelte";
   import Footer from "../shared/Footer.svelte";
   import Navbar from "../shared/Navbar.svelte";
 </script>
 
-<Home />
+<!-- <Home /> -->
+<SignUp />
+<!-- <JobPost /> -->
 <!-- <Navbar /> -->
 <!-- <Footer /> -->
 <!-- Fetch this common components in the webpages -->
