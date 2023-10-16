35b8452bd621498e555f518219e351bfa6fdb4bc
Mansi <mansisharma209aps@gmail.com>
2023-10-17 02:17:24 +0530

    Job post and Job apply page

---
+++ src/routes/jobapply/+page.svelte
@@ -0,0 +1,186 @@
+<script>
+    import Footer from "../../shared/Footer.svelte";
+    import Navbar from "../../shared/Navbar.svelte";
+
+    let file = null;
+
+    let numberOfOpenings = 0;
+
+    function increment() {
+        numberOfOpenings += 1;
+    }
+
+    function decrement() {
+        if (numberOfOpenings > 0) {
+            numberOfOpenings -= 1;
+        }
+    }
+
+    function handleSubmit() {
+        // Handle form submission logic here
+    }
+</script>
+
+<!-- NAVBAR -->
+<Navbar />
+
+<!-- JOB APPLY FORM -->
+<section class="max-w-4xl p-6 mx-auto rounded-md shadow-md my-10 bg-gray-100">
+    <!-- HEADING -->
+    <h1 class="font-bold text-3xl text-gray-900 text-center">Apply Job</h1>
+    <form on:submit|preventDefault={handleSubmit}>
+        <div>
+            <!-- FIRST AND LAST NAME -->
+            <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+                <!-- FIRST NAME -->
+                <div>
+                    <label class="text-gray-800 font-semibold" for="username"
+                        >First Name</label
+                    >
+                    <div class="flex">
+                        <input
+                            type="text"
+                            class="w-full pl-4 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+                            placeholder="John"
+                        />
+                    </div>
+                </div>
+
+                <!-- LAST NAME -->
+                <div>
+                    <label class="text-gray-800 font-semibold" for="username"
+                        >Last Name</label
+                    >
+                    <div class="flex">
+                        <input
+                            type="text"
+                            class="w-full pl-4 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+                            placeholder="John"
+                        />
+                    </div>
+                </div>
+            </div>
+
+            <!-- EMAIL ID AND PHONE NUMBER -->
+            <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+                <!-- EMAIL ID -->
+                <div>
+                    <label
+                        class="text-gray-800 font-semibold"
+                        for="emailAddress">Email ID</label
+                    >
+                    <div class="flex">
+                        <input
+                            type="email"
+                            class="w-full pl-4 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+                            placeholder="johnsmith@example.com"
+                        />
+                    </div>
+                </div>
+
+                <!-- PHONE NUMBER -->
+                <div>
+                    <div>
+                        <label class="text-gray-800 font-semibold" for="phone"
+                            >Phone Number</label
+                        >
+                    </div>
+
+                    <div class="flex items-center space-x-2">
+                        <div
+                            class="text-gray-600 py-1 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+                        >
+                            <span
+                                class="text-gray-800 mx-3 text-xl font-semibold"
+                                >+91</span
+                            >
+                        </div>
+                        <input
+                            id="phone"
+                            type="tel"
+                            class="text-gray-600 w-full pl-4 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+                            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                            placeholder="Enter your phone number"
+                        />
+                    </div>
+                </div>
+            </div>
+
+            <!-- RESUME UPLOAD -->
+            <div class="mt-4">
+                <label
+                    class="text-gray-800 font-semibold mb-3"
+                    for="Resume upload">Resume</label
+                >
+                <div
+                    class="bg-white mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-500 border-dashed rounded-md"
+                >
+                    <div class="space-y-1 text-center">
+                        <svg
+                            class="mx-auto h-12 w-12 text-gray-600"
+                            stroke="currentColor"
+                            fill="none"
+                            viewBox="0 0 48 48"
+                            aria-hidden="true"
+                        >
+                            <path
+                                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            />
+                        </svg>
+                        <div class="flex text-sm text-gray-600">
+                            <label
+                                for="file-upload"
+                                class="relative cursor-pointer bg-white rounded-md font-medium text-gray-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+                            >
+                                <span class="">Upload a file</span>
+                                <input
+                                    id="file-upload"
+                                    name="file-upload"
+                                    type="file"
+                                    class="sr-only"
+                                    bind:files={file}
+                                />
+                            </label>
+                            <p class="pl-1 text-gray-600">or drag and drop</p>
+                        </div>
+                        <p class="text-xs text-gray-600">
+                            PDF, PNG, JPG, GIF up to 10MB
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- ANSWER TO QUESTION -->
+            <div class="mt-4">
+                <label class="text-gray-800 font-semibold pb-3" for="answer"
+                    >Why do you want to join this organisation? (sample
+                    question)</label
+                >
+                <textarea
+                    id="answer"
+                    class="w-full pl-4 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500 mt-2"
+                    placeholder="I want to join this organisation..."
+                />
+            </div>
+
+            <!-- JOB APPLY BUTTON -->
+            <div class="flex justify-end mt-6">
+                <button
+                    class="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold"
+                    >APPLY</button
+                >
+            </div>
+        </div>
+    </form>
+</section>
+
+<!-- FOOTER -->
+<Footer />
+
+<style>
+    @import url("https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.3.45/css/materialdesignicons.min.css");
+    /* Add any additional styles you need */
+</style>
--- src/routes/jobpost/+page.svelte
+++ src/routes/jobpost/+page.svelte
@@ -13,6 +13,18 @@
     let textArea = "";
     let file = null;
 
+    let numberOfOpenings = 0;
+
+    function increment() {
+        numberOfOpenings += 1;
+    }
+
+    function decrement() {
+        if (numberOfOpenings > 0) {
+            numberOfOpenings -= 1;
+        }
+    }
+
     function handleSubmit() {
         // Handle form submission logic here
     }
@@ -21,177 +33,134 @@
 <!-- NAVBAR -->
 <Navbar />
 
-<section
-    class="max-w-4xl p-6 mx-auto bg-gray-800 rounded-md shadow-md dark:bg-gray-800 my-20"
->
-    <h1 class="text-xl font-bold text-white capitalize dark:text-white">
-        Account settings
-    </h1>
+<!-- POST JOB FORM -->
+<section class="max-w-4xl p-6 mx-auto rounded-md shadow-md my-10 bg-gray-100">
+    <!-- HEADING -->
+    <h1 class="font-bold text-3xl text-gray-900 text-center">Post Job</h1>
     <form on:submit|preventDefault={handleSubmit}>
-        <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+        <div>
+            <!-- JOB TITLE -->
             <div>
-                <label class="text-white dark:text-gray-200" for="username"
-                    >Username</label
+                <label class="text-gray-800 font-semibold" for="title"
+                    >Job Title</label
                 >
-                <input
-                    id="username"
-                    type="text"
-                    class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-                    bind:value={username}
-                />
+                <div class="flex">
+                    <input
+                        type="text"
+                        class="w-full pl-4 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+                        placeholder="SDE"
+                    />
+                </div>
             </div>
 
-            <div>
-                <label class="text-white dark:text-gray-200" for="emailAddress"
-                    >Email Address</label
-                >
-                <input
-                    id="emailAddress"
-                    type="email"
-                    class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-                    bind:value={emailAddress}
-                />
+            <!-- JOB TYPE AND JOB END DATE -->
+            <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+                <!-- JOB TYPE -->
+                <div>
+                    <label class="text-gray-800 font-semibold" for="type"
+                        >Job Type</label
+                    >
+                    <div class="flex">
+                        <input
+                            type="type"
+                            class="w-full pl-4 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+                            placeholder="Full-Time"
+                        />
+                    </div>
+                </div>
+
+                <!-- JOB END DATE -->
+                <div>
+                    <label class="text-gray-800 font-semibold" for="date"
+                        >Job End Date</label
+                    >
+                    <input
+                        id="date"
+                        type="date"
+                        class="text-gray-600 w-full pl-4 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+                        bind:value={date}
+                    />
+                </div>
             </div>
 
-            <div>
-                <label class="text-white dark:text-gray-200" for="password"
-                    >Password</label
-                >
-                <input
-                    id="password"
-                    type="password"
-                    class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-                    bind:value={password}
-                />
-            </div>
-
-            <div>
-                <label
-                    class="text-white dark:text-gray-200"
-                    for="passwordConfirmation">Password Confirmation</label
-                >
-                <input
-                    id="passwordConfirmation"
-                    type="password"
-                    class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-                    bind:value={passwordConfirmation}
-                />
-            </div>
-
-            <div>
-                <label class="text-white dark:text-gray-200" for="color"
-                    >Color</label
-                >
-                <input
-                    id="color"
-                    type="color"
-                    class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-                    bind:value={color}
-                />
-            </div>
-
-            <div>
-                <label class="text-white dark:text-gray-200" for="city"
-                    >Select</label
-                >
-                <select
-                    class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-                    bind:value={selectedCity}
-                >
-                    <option>Surabaya</option>
-                    <option>Jakarta</option>
-                    <option>Tangerang</option>
-                    <option>Bandung</option>
-                </select>
-            </div>
-
-            <div>
-                <label class="text-white dark:text-gray-200" for="range"
-                    >Range</label
-                >
-                <input
-                    id="range"
-                    type="range"
-                    class="block w-full py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-                    bind:value={rangeValue}
-                />
-            </div>
-
-            <div>
-                <label class="text-white dark:text-gray-200" for="date"
-                    >Date</label
-                >
-                <input
-                    id="date"
-                    type="date"
-                    class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-                    bind:value={date}
-                />
-            </div>
-
-            <div>
-                <label class="text-white dark:text-gray-200" for="textarea"
-                    >Text Area</label
+            <!-- JOB DESCRIPTION -->
+            <div class="mt-4">
+                <label class="text-gray-800 font-semibold" for="textarea"
+                    >Job Description</label
                 >
                 <textarea
-                    id="textarea"
-                    class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-                    bind:value={textArea}
+                    id="description"
+                    class="w-full pl-4 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+                    placeholder="Work on developing and enhancing data models, algorithms, ...
+                    "
                 />
             </div>
 
-            <div>
-                <!-- <label class="block text-sm font-medium text-white">
-                    Image
-                </label> -->
-                <div
-                    class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md"
-                >
-                    <div class="space-y-1 text-center">
-                        <svg
-                            class="mx-auto h-12 w-12 text-white"
-                            stroke="currentColor"
-                            fill="none"
-                            viewBox="0 0 48 48"
-                            aria-hidden="true"
+            <!-- STIPEND AND NUMBER OF OPENING(S)-->
+            <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+                <!-- STIPEND -->
+                <div>
+                    <div>
+                        <label
+                            class="text-gray-800 font-semibold"
+                            for="password">Stipend</label
                         >
-                            <path
-                                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                                stroke-width="2"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                            />
-                        </svg>
-                        <div class="flex text-sm text-gray-600">
-                            <label
-                                for="file-upload"
-                                class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
-                            >
-                                <span class="">Upload a file</span>
-                                <input
-                                    id="file-upload"
-                                    name="file-upload"
-                                    type="file"
-                                    class="sr-only"
-                                    bind:files={file}
-                                />
-                            </label>
-                            <p class="pl-1 text-white">or drag and drop</p>
+                    </div>
+                    <div class="flex items-center space-x-2">
+                        <div
+                            class="text-gray-600 py-1 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+                        >
+                            <span class="text-gray-800 mx-3 text-2xl">₹</span>
                         </div>
-                        <p class="text-xs text-white">
-                            PNG, JPG, GIF up to 10MB
-                        </p>
+                        <input
+                            id="stipend"
+                            type="number"
+                            class="text-gray-600 w-full pl-4 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+                            placeholder="50000"
+                        />
+                    </div>
+                </div>
+
+                <!-- JOB OPENING(S) -->
+                <div>
+                    <label class="text-gray-800 font-semibold" for="password"
+                        >Number of Opening(s)</label
+                    >
+                    <div>
+                        <button
+                            class="bg-white pb-2 px-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500 text-2xl font-semibold"
+                            on:click={increment}>+</button
+                        >
+                        <span class="mx-4 text-2xl font-semibold"
+                            >{numberOfOpenings}</span
+                        >
+                        <button
+                            class="bg-white pb-2 px-3 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500 font-semibold text-2xl"
+                            on:click={decrement}>-</button
+                        >
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="flex justify-end mt-6">
-            <button
-                type="submit"
-                class="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-pink-500 rounded-md hover:bg-pink-700 focus:outline-none focus:bg-gray-600"
-                >Post Job</button
-            >
+            <!-- QUESTION -->
+            <div class="mt-4">
+                <label class="text-gray-800 font-semibold" for="textarea"
+                    >Question</label
+                >
+                <textarea
+                    id="textarea"
+                    class="w-full pl-4 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+                    placeholder="Why should we hire you?"
+                />
+            </div>
+
+            <!-- POST JOB -->
+            <div class="flex justify-end mt-6">
+                <button
+                    class="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold"
+                    >POST JOB</button
+                >
+            </div>
         </div>
     </form>
 </section>
@@ -200,9 +169,6 @@
 <Footer />
 
 <style>
-    /* Dark mode styles */
-    :global(body.dark) {
-        background-color: #333;
-        color: #fff;
-    }
+    @import url("https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.3.45/css/materialdesignicons.min.css");
+    /* Add any additional styles you need */
 </style>
